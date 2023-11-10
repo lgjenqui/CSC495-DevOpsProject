7303f2c74e7ecde52de28fc55dea1d50de038112
Jubitta John <jjohn6@ncsu.edu>
2023-11-10 17:24:13 -0500

    Added code coverage threshold
    Signed-off-by: Jubitta John <jjohn6@ncsu.edu>

--- coffee-project/jest.config.js
+++ coffee-project/jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
     collectCoverage: true,
     coverageReporters: ['text', 'cobertura'],
+    coverageThreshold: {
+        global: {
+          branches: 80,
+          functions: 80,
+          lines: 80,
+          statements: 80,
+        },
+      },
 }
